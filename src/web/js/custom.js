9dc5cd3956384975d6b2e36fe6304fc579b73806
L Shaf <shafry@mimicreative.net>
2018-05-24 15:04:59 +0700

    REMOVE THE ANNOYING SCRIPT

--- src/web/js/custom.js
+++ src/web/js/custom.js
@@ -202,25 +202,4 @@ $(document).ready(function () {
 
         });
     });
-
-
-    // Select2 Initialize
-    // Select2 without the search
-    if ($.fn.select2) {
-        $('.select2').select2({
-            minimumResultsForSearch: Infinity
-        });
-
-        // Select2 by showing the search
-        $('.select2-show-search').select2({
-            minimumResultsForSearch: ''
-        });
-
-        // Select2 with tagging support
-        $('.select2-tag').select2({
-            tags: true,
-            tokenSeparators: [',', ' ']
-        });
-    }
-
 });
