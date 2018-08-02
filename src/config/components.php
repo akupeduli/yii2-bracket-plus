802ebb7c94a62b3bf9ec22ad74937701532ab61a
L Shaf <shafry@mimicreative.net>
2018-08-02 11:24:53 +0700

    bootstrap4 no longer use popper.js

--- src/config/components.php
+++ src/config/components.php
@@ -2,7 +2,6 @@
 
 use akupeduli\bracket\assets\plugins\DataTableAsset;
 use yii\bootstrap4\BootstrapPluginAsset;
-use yii\bootstrap4\PopperAsset;
 use yii\web\JqueryAsset;
 use yii\web\View;
 
@@ -13,17 +12,9 @@ return [
         ],
     ],
 
-    PopperAsset::class => [
-        'sourcePath' => null,
-        'js' => [
-            'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.js'
-        ],
-    ],
-
     BootstrapPluginAsset::class => [
         'depends' => [
-            JqueryAsset::class,
-            PopperAsset::class
+            JqueryAsset::class
         ],
     ],
 
