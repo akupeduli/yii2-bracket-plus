6579b31f20eec9376d9ee9782ea43f45fec0104e
L Shaf <shafry2008@gmail.com>
2019-04-12 15:31:13 +0700

    🥕 update wrong layout login + add css

--- src/views/auth/login.php
+++ src/views/auth/login.php
@@ -17,61 +17,62 @@ if (!($loginForm instanceof LoginForm) and !is_subclass_of($loginForm, LoginForm
 }
 ?>
 
-<div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
-    <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white rounded shadow-base">
-        <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> bracket <span class="tx-info">plus</span> <span class="tx-normal">]</span></div>
-        <div class="tx-center mg-b-60">The Admin Template For Perfectionist</div>
-        <?php
-        $form = ActiveForm::begin([
-            "errorCssClass" => "has-danger",
-            "options" => [
-                "id" => "loginForm",
-            ],
-            "fieldConfig" => [
-                "template" => "{input}\n{error}",
-                "inputOptions" => [
-                    "class" => "form-control",
+    <div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
+        <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white rounded shadow-base">
+            <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> bracket <span class="tx-info">plus</span> <span class="tx-normal">]</span></div>
+            <div class="tx-center mg-b-60">The Admin Template For Perfectionist</div>
+            <?php
+            $form = ActiveForm::begin([
+                "errorCssClass" => "has-danger",
+                "options" => [
+                    "id" => "loginForm",
                 ],
-                "errorOptions" => [
-                    "class" => "form-control-feedback",
-                ],
-            ],
-        ]);
-        if ($loginForm->loginWith === LoginForm::WITH_USERNAME) {
-            echo $form->field($loginForm, "username", [
-                "inputOptions" => [
-                    "placeholder" => $loginForm->getAttributeLabel("username"),
+                "fieldConfig" => [
+                    "template" => "{input}\n{error}",
+                    "inputOptions" => [
+                        "class" => "form-control",
+                    ],
+                    "errorOptions" => [
+                        "class" => "form-control-feedback",
+                    ],
                 ],
             ]);
-        } else if ($loginForm->loginWith === LoginForm::WITH_EMAIL) {
+            if ($loginForm->loginWith === LoginForm::WITH_USERNAME) {
+                echo $form->field($loginForm, "username", [
+                    "inputOptions" => [
+                        "placeholder" => $loginForm->getAttributeLabel("username"),
+                    ],
+                ]);
+            } else if ($loginForm->loginWith === LoginForm::WITH_EMAIL) {
+                echo $form->field($loginForm, "email", [
+                    "inputOptions" => [
+                        "placeholder" => $loginForm->getAttributeLabel("email"),
+                    ],
+                ]);
+            }
+            
             $forgotUrl = "";
             if (!is_null($loginForm->forgotPasswordUrl)) {
                 $forgotUrl = Html::a("Forgot Password", $loginForm->forgotPasswordUrl, [
                     "class" => 'tx-info tx-12 d-block mg-t-10'
                 ]);
             }
-            echo $form->field($loginForm, "email", [
+            
+            echo $form->field($loginForm, "password", [
                 "template" => "{input}\n{hint}\n{error}\n" . $forgotUrl,
                 "inputOptions" => [
-                    "placeholder" => $loginForm->getAttributeLabel("email"),
+                    "placeholder" => $loginForm->getAttributeLabel("password"),
                 ],
-            ]);
-        }
-    
-        echo $form->field($loginForm, "password", [
-            "inputOptions" => [
-                "placeholder" => $loginForm->getAttributeLabel("password"),
-            ],
-        ])->passwordInput();
-        ?>
-        <button type="submit" class="btn btn-info btn-block">Sign In</button>
-        <?php if (!is_null($loginForm->registerUrl)): ?>
-            <div class="mg-t-60 tx-center">
-                Not yet a member? <a href="<?= Url::to($loginForm->registerUrl) ?>" class="tx-info">Sign Up</a>
-            </div>
-        <?php endif; ?>
-        <?php ActiveForm::end() ?>
-    </div><!-- login-wrapper -->
-</div><!-- d-flex -->
+            ])->passwordInput();
+            ?>
+            <button type="submit" class="btn btn-info btn-block">Sign In</button>
+            <?php if (!is_null($loginForm->registerUrl)): ?>
+                <div class="mg-t-60 tx-center">
+                    Not yet a member? <a href="<?= Url::to($loginForm->registerUrl) ?>" class="tx-info">Sign Up</a>
+                </div>
+            <?php endif; ?>
+            <?php ActiveForm::end() ?>
+        </div><!-- login-wrapper -->
+    </div><!-- d-flex -->
 
 <?php
