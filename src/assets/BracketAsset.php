e7d102ce4a4befcbd56d6b1e889854bb69ce682f
L Shaf <shafry2008@gmail.com>
2019-04-12 15:32:47 +0700

    🗡 add adjusted css to asset

--- src/assets/BracketAsset.php
+++ src/assets/BracketAsset.php
@@ -2,40 +2,18 @@
 
 namespace akupeduli\bracket\assets;
 
-use akupeduli\bracket\Bracket;
-use rmrevin\yii\fontawesome\AssetBundle as FABundle;
-use rmrevin\yii\ionicon\AssetBundle as IonBundle;
+use akupeduli\bracket\assets\core\StyleAsset;
 use yii\web\AssetBundle;
-use yii\web\YiiAsset;
 
 class BracketAsset extends AssetBundle
 {
-    public $publishOptions = [
-        'only' => [
-            "css/*"
-        ]
+    public $sourcePath = "@akupeduli/bracket/web";
+    
+    public $js = [
+        'js/bracket.js'
     ];
     
-    public $css = [
-        "css/bracket.css",
+    public $depends =  [
+        StyleAsset::class
     ];
-    
-    public $depends = [
-        FABundle::class,
-        IonBundle::class
-    ];
-    
-    public function init()
-    {
-        /** @var Bracket $bracket */
-        $bracket          = Bracket::getComponent();
-        $this->sourcePath = $bracket->assetPath;
-        if ($bracket->theme !== Bracket::THEME_DEFAULT) {
-            $themeCss  = "css/bracket." . $bracket->theme;
-            $themeCss .= (YII_ENV_DEV ? "" : ".min") . ".css";
-            $this->css[] = $themeCss;
-        }
-        
-        parent::init();
-    }
 }
---
+++ src/assets/core/StyleAsset.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace akupeduli\bracket\assets\core;
+
+use akupeduli\bracket\Bracket;
+use rmrevin\yii\fontawesome\AssetBundle as FABundle;
+use rmrevin\yii\ionicon\AssetBundle as IonBundle;
+use yii\web\AssetBundle;
+
+class StyleAsset extends AssetBundle
+{
+    public $publishOptions = [
+        'only' => [
+            "css/*"
+        ]
+    ];
+    
+    public $css = [
+        "css/bracket.css",
+    ];
+    
+    public $depends = [
+        FABundle::class,
+        IonBundle::class
+    ];
+    
+    public function init()
+    {
+        /** @var Bracket $bracket */
+        $bracket          = Bracket::getComponent();
+        $this->sourcePath = $bracket->assetPath;
+        if ($bracket->theme !== Bracket::THEME_DEFAULT) {
+            $themeCss  = "css/bracket." . $bracket->theme;
+            $themeCss .= (YII_ENV_DEV ? "" : ".min") . ".css";
+            $this->css[] = $themeCss;
+        }
+        
+        parent::init();
+    }
+}
